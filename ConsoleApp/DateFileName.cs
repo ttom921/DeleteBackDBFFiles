eb433811eaefccc68c3ad904ed999e443bba23fc
ttom921 <ttom921@hotmail.com>
2018-11-26 10:43:39 +0800

    加入第一版的刪除

---
+++ ConsoleApp/DateFileName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class DateFileName
+    {
+        public string GenDataFileName()
+        {
+            string ret = null;
+            DateTime dt = DateTime.Now.AddDays(-7);
+            string strdt = dt.ToString("_yyyy_MM_dd_");
+            return ret= strdt;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateFileName dateFileName = new DateFileName();
+            string dtfile = dateFileName.GenDataFileName();
+            DeleteFiles(dtfile);
+            //Console.WriteLine("dtfile->" + dtfile);
+            Console.WriteLine("job done");
+            Console.Read();
+        }
+        private static void DeleteFiles(string dtfile)
+        {
+            try
+            {
+                string directoryPath = @"D:\SQLBackup";
+                var dir = new DirectoryInfo(directoryPath);
+                string patrernfile = $"*{dtfile}*";
+                foreach (var file in dir.EnumerateFiles(patrernfile))
+                {
+                    string msg = $"delete file->{file.Name}";
+                    file.Delete();
+                    Console.WriteLine(msg);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(nameof(DeleteFiles) + " " + ex.Message);
+            }
+        }
+    }
+}
